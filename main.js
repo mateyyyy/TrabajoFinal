8cfb93ac7d0b9f9e79c353f480eb0b25503e084c
Mati <65136286+mateyyyy@users.noreply.github.com>
2023-11-28 23:35:20 -0300

    V1.1
    Agregue programacion estructurada separando el codigo por funciones

--- main.js
+++ main.js
@@ -1,17 +1,78 @@
-import prompt from 'prompt-sync';
 import TasksList from './task.js';
+import prompt from 'prompt-sync';
 const promptSync = prompt();
 
 function esperaEnter() {
-  prompt('Presione Enter para continuar...');
+  promptSync('Presione Enter para continuar...');
+}
+
+function menu(){
+  console.log(`
+    ==============================
+              TO-DO List
+    ==============================
+   
+    1. Ver tareas.
+    2. Buscar una tarea.
+    3. Agregar una tarea.
+    0. Salir.
+    `);
 }
 
 function main() {
-  let op, titulo, descripcion, estado;
+  var op, titulo,descripcion, estado;
   const lista = new TasksList();
-  lista.addTask('Hola', false);
-  lista.addTask('chau', false);
-  lista.mostrarTareas();
-}
+  do
+    {
+        process.stdout.write('\x1B[2J\x1B[0f');
+        menu();
+        op = promptSync('Ingrese una opcion : ');
+        process.stdout.write('\x1B[2J\x1B[0f');
+        switch(op)
+        {
+            case '1':
+                if(lista.estaVacia()){
+                  console.log('No hay tareas...');
+                }
+                else
+                {
+                  lista.mostrarTareas();
+                }
+                esperaEnter();
+            break;
+
+
+            case '2':
+                if(!lista.estaVacia()){
+                    titulo = promptSync('Escriba el nombre de la tarea : ');
+                    lista.buscarTarea(titulo);
+                }
+                else
+                {
+                    console.log('No hay tareas cargadas...');
+                }
+                esperaEnter();
+            break;
+
+
+            case '3':
+                titulo = promptSync('Ingrese el titulo : ');
+                descripcion = promptSync('Ingrese la descripcion : ');
+                estado = promptSync(`Ingrese el estado : [P]endiente, [E]n curso, [T]erminada : `);
+                while(estado.toLowerCase()!='p' && estado.toLowerCase()!='e' && estado.toLowerCase()!='t')
+                {
+                    console.log('Valor incorrecto, ingrese nuevamente....');
+                    estado = promptSync(`Ingrese el estado : [P]endiente, [E]n curso, [T]erminada : `);
+                }
+                let vencimiento = promptSync('Ingrese el vencimiento YYYY/MM/DD : ');
+                let dificultad = promptSync('Ingrese la dificultad {1,2,3} : ');
+                lista.addTask(titulo, descripcion, estado, vencimiento, dificultad);
+            break;
+        }
+    }
+    while(op!=0);
+    console.log('Hasta la proxima!');
+};
+
 
 export default main;
--- task.js
+++ task.js
@@ -1,32 +1,62 @@
-//PARA LA DEFINICION DE LAS TAREAS USO EL PARADIGMA ORIENTADO A OBJETOS POR QUE 
-
+// Programación Orientada a Objetos (POO)
 class Task {
+  // Definición de una clase con propiedades y método constructor
   titulo = '';
   descripcion = '';
-  status = false;
+  estado = '';
+  vencimiento = '';
+  dificultad = 0;
 
-  constructor(titulo, descripcion, status) {
+
+  constructor(titulo, descripcion, estado, vencimiento, dificultad) {
     this.titulo = titulo;
     this.descripcion = descripcion;
-    this.status = status;
+    this.estado = estado;
+    this.vencimiento = vencimiento;
+    this.dificultad = dificultad;
+  }
+
+  mostrarTarea(){
+    console.log('Titulo : ', this.titulo);
+    console.log('Descripcion : ', this.descripcion);
+    console.log('Estado : ', this.estado);
+    console.log('Vencimiento : ', this.vencimiento);
+    console.log('Dificultad : ', this.dificultad);
   }
 }
 
+// (POO)
 class TasksList {
   constructor() {
     this.tasks = [];
   }
 
-  addTask(titulo, descripcion, status) {
-    this.tasks.push(new Task(titulo, descripcion, status));
+  addTask(titulo, descripcion, estado, vencimiento, dificultad) {
+    this.tasks.push(new Task(titulo, descripcion, estado, vencimiento, dificultad));
   }
 
-  mostrarTareas()
-  {
+  mostrarTareas() {
+    // Iteración sobre la lista de tareas, parte de la programacion estructurada
     for (let i = 0; i < this.tasks.length; i++) {
-      console.log(this.tasks[i].titulo);
+      console.log('Tarea ', i+1);
+      console.log(this.tasks[i].mostrarTarea());
+      console.log('');
+    }
+  }
+
+  estaVacia(){
+    return this.tasks.length == 0;
+  }
+
+  buscarTarea(titulo){
+    for (let i = 0; i < this.tasks.length; i++) {
+      if(this.tasks[i].titulo == titulo){
+        this.tasks[i].mostrarTarea();
+        console.log('');
+      }
     }
   }
 }
 
+// Programación Orientada a Objetos (OOP)
 export default TasksList;
